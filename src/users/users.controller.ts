0dc2f04ca6bb09638873c6cd1c0a22a08e20097d
arthur <arthur.broudoux@gmail.com>
2024-01-07 23:51:23 +0100

    Add PATH Method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Get, Param, Controller } from "@nestjs/common";
+import { Get, Param, Controller, Patch, Body } from "@nestjs/common";
 
 import { UsersService } from "@/users/users.service";
+import { UpdateUserDto } from "@/auth/dto/update-user.dto";
 
 
 @Controller('users')
@@ -14,8 +15,13 @@ export class UsersController {
     };
 
     @Get('/:userId')
-    getUser(@Param('userId') userId : string ) {
+    getUser(@Param('userId') userId : string) {
         return this.usersService.getUser({ userId });
+    };
+
+    @Patch('/:userId')
+    updateUser(@Param('userId') userId : string, @Body() updateUserDto : UpdateUserDto) {
+        return this.usersService.updateUser({ userId }, updateUserDto);
     };
 
 };
